dadb5b66330a76acb417e4d5be2ec1be74cda5c8
Thaissg2 <thaissg2@al.insper.edu.br>
2020-11-20 12:32:52 -0300

    tentativa java

--- java/f1_t6.js
+++ java/f1_t6.js
@@ -1,1 +1,15 @@
-document.addEventListener('DOMContantLoaded', function())
+document.addEventListener('DOMContentLoaded', function() {
+    function before() {
+    }
+
+    function after() {
+        a = document.querySelector('p')
+        a.innerHTML = 'A viagem do seu Aumigo foi finalizada! Clique aqui para avaliar o seu cãotorista'
+    }
+    a = document.querySelector('a')
+
+    a.addEventListener('click', function() {
+        before()
+        setTimeout(after, 1)
+    })
+})
